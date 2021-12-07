3705a3981ef71d5a2a9aac13cc77da64a4ce0f47
Ángel Rubén Valdeolmos Carmona <angval@eit.es>
2021-12-07 13:44:48 +0100

    input fields

---
+++ lib/pages/input_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String? _nombre;
+  String? _email;
+  String? _date;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Input'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: <Widget>[
+          _createInput(),
+          const Divider(),
+          _createEmail(),
+          const Divider(),
+          _createDate(),
+          const Divider(),
+          _createPerson(),
+        ],
+      ),
+    );
+  }
+
+  Widget _createInput() {
+    return TextField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
+          counter: const Text('Letras 0'),
+          hintText: 'Pon tu nombre',
+          labelText: 'Nombre'),
+      onChanged: (value) {
+        setState(() {
+          _nombre = value;
+        });
+      },
+    );
+  }
+
+  Widget _createPerson() {
+    return ListTile(
+      title: Text('El nombre es  $_nombre'),
+      subtitle: Text('El correo es  $_email'),
+    );
+  }
+
+  Widget _createEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
+          hintText: 'Email',
+          labelText: 'Email',
+          suffixIcon: const Icon(Icons.alternate_email),
+          icon: const Icon(Icons.email)),
+      onChanged: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+    );
+  }
+
+  Widget _createDate() {}
+}
